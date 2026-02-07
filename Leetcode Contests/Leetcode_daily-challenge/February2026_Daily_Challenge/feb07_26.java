7536929b2aaa208763d91ace4c7f094c2bf07f87
Prittam <prittamraj2018282@gmail.com>
2026-02-07 22:16:32 +0530

    07-02-2026

---
+++ Leetcode Contests/Leetcode_daily-challenge/February2026_Daily_Challenge/feb07_26.java
@@ -0,0 +1,26 @@
+public class feb07_26 {
+    public static int minimumDeletions(String s) {
+        int dp[] = new int[s.length()+1];
+        dp[0] = 0;
+        int freq[] = new int[2];
+        for(int i=1; i<=s.length(); i++){
+            if(s.charAt(i-1) == 'a'){
+                if(freq[1] > 0){
+                    dp[i] = Math.min(dp[i-1] + 1, freq[1]);
+                } else {
+                    dp[i] = dp[i-1];
+                }
+                freq[0]++;
+            } else {
+                freq[1]++;
+                dp[i] = dp[i-1];
+            }
+        }
+        return dp[s.length()];
+    }
+    public static void main(String[] args) {
+        String s = "aababbab";
+        String s1 = "bbaaaaabb";
+        System.out.println(minimumDeletions(s) + " " + minimumDeletions(s1));
+    }
+}
