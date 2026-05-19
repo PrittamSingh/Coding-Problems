4f3f8110de331b1e6912f7a5c04ce04f9739d992
Prittam <prittamraj2018282@gmail.com>
2026-05-19 22:44:19 +0530

    19-05-2026

---
+++ Leetcode Contests/Leetcode_daily-challenge/May2026_Daily_Challenge/may19_26.java
@@ -0,0 +1,42 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class may19_26 {
+    public static int getCommon(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums1){
+            set.add(num);
+        }
+        for(int num : nums2){
+            if(set.contains(num)){
+                return num;
+            }
+        }
+        return -1;
+
+
+
+
+        // int n = nums1.length;
+        // int m = nums2.length;
+        // int i = 0;
+        // int j = 0;
+        // while(i < n && j < m){
+        //     if(nums1[i] == nums2[j]){
+        //         return nums1[i];
+        //     }
+        //     else if(nums1[i] > nums2[j]){
+        //         j++;
+        //     }
+        //     else if(nums1[i] < nums2[j]){
+        //         i++;
+        //     }
+        // }
+        // return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3};
+        int[] nums2 = {2,4};
+        System.out.println(getCommon(nums1, nums2));
+    }
+}
