af0c9068fd8826419ee08852035f100d3cd82fb2
Prittam <prittamraj2018282@gmail.com>
2026-05-22 23:28:30 +0530

    22-05-2026

---
+++ Leetcode Contests/Leetcode_daily-challenge/May2026_Daily_Challenge/may22_26.java
@@ -0,0 +1,45 @@
+public class may22_26 {
+    public static int search(int[] nums, int target) {
+        // int n = nums.length;
+        // for(int i = 0 ; i < n ; i++){
+        //     if(nums[i] == target){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+
+        // BINARY SEARCH
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target < nums[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            else{
+                if(nums[mid] < target && target <= nums[high]){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,7,0,1,2};
+        int target = 0;
+        System.out.println(search(nums, target));
+    }
+}
