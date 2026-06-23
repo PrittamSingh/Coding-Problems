65c1bf5279562bea201d4a143adee14214231f1e
Prittam <prittamraj2018282@gmail.com>
2026-06-23 22:07:00 +0530

    23-06-2026

---
+++ Leetcode Contests/Leetcode_daily-challenge/June2026_Daily_Challenge/june23_26.java
@@ -0,0 +1,35 @@
+public class june23_26 {
+    public static int zigZagArrays(int n, int l, int r) {
+        int k = r - l + 1;
+        int MOD = 1000000007;
+        int[][] dp = new int[k + 1][2];
+        for(int v = 1 ; v <= k ; v++){
+            dp[v][0] = k - v; 
+            dp[v][1] = v - 1; 
+        }
+        for(int i = 3 ; i <= n ; i++){
+            int[][] nextDp = new int[k + 1][2];
+            long runSum0 = 0;
+            for(int v = 1 ; v <= k ; v++){
+                nextDp[v][1] = (int) runSum0;
+                runSum0 = (runSum0 + dp[v][0]) % MOD;
+            }
+            long runSum1 = 0;
+            for(int v = k ; v >= 1 ; v--){
+                nextDp[v][0] = (int) runSum1;
+                runSum1 = (runSum1 + dp[v][1]) % MOD;
+            }  
+            dp = nextDp;
+        }
+        long total = 0;
+        for(int v = 1 ; v <= k ; v++){
+            total = (total + dp[v][0]) % MOD;
+            total = (total + dp[v][1]) % MOD;
+        }
+        return (int)total;
+    }
+    public static void main(String[] args) {
+        int  n = 3, l = 4, r = 5;
+        System.out.println(zigZagArrays(n, l, r));
+    }
+}
