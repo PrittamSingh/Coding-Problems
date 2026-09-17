1ce13e7938740082c59b9e995f81786e7cefa4e6
Prittam <prittamraj2018282@gmail.com>
2026-09-17 16:09:34 +0530

    17-09-2026

---
+++ Leetcode Contests/Leetcode_daily-challenge/September2026_Daily_Challenge/sept17_26.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class sept17_26 {
+    public static int minSumOfLengths(int[] arr, int target) {
+        int n = arr.length;
+        int i = 0;
+        int j = 0;
+        int currSum = 0;
+        int[] minBestLenTillIdx = new int[n];
+        Arrays.fill(minBestLenTillIdx, Integer.MAX_VALUE);
+        int result = Integer.MAX_VALUE;
+        int bestMinLen = Integer.MAX_VALUE;
+        while(j < n){
+            currSum += arr[j];
+            while(i < j && currSum > target){
+                currSum -= arr[i];
+                i++;
+            }
+            if(currSum == target){
+                int len = j - i + 1; 
+                if(i > 0 && minBestLenTillIdx[i - 1] != Integer.MAX_VALUE){
+                    result = Math.min(result, len + minBestLenTillIdx[i - 1]);
+                }
+                bestMinLen = Math.min(bestMinLen, len);
+            }
+            minBestLenTillIdx[j] = bestMinLen;
+            j++;
+        }
+        return result == Integer.MAX_VALUE ? -1 : result;
+    }
+    public static void main(String[] args) {
+        int[] arr = {3,2,2,4,3};
+        int target = 3;
+        int[] arr2 = {4,3,2,6,2,3,4};
+        int target2 = 6;
+        System.out.println(minSumOfLengths(arr, target) + " " + minSumOfLengths(arr2, target2));
+    }
+}
